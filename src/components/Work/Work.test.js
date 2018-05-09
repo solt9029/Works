25bbe21b4b56bfcaa1961f8bb1f97d2cee66ffc4
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-05-09 15:43:00 +0900

    Work

--- src/components/Work/Work.test.js
+++ src/components/Work/Work.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Work from './Work';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  ReactDOM.render(<Work />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
