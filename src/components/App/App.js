0876b181645da05fda97ff9dc9be808123f9d781
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-05-09 11:21:45 +0900

    react-bootstrap 導入

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        Welcome to React!
-      </div>
+      <Button>button</Button>
     );
   }
 }
