e36baccde9a1275113909b85415978f0934c5651
Kenshi Shiode <atemu1212@yahoo.co.jp>
2018-05-09 16:56:09 +0900

    メソッド修正

--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -3,14 +3,10 @@ import './Work.css';
 import { Card, Col, CardImg, CardText, CardBody } from 'reactstrap';
 
 export default class Work extends Component {
-  openUrl() {
-    window.open(this.props.url, '_blank');
-  }
-
   render() {
     return (
       <Col lg={4} md={6} sm={12} xs={12}>
-        <Card onClick={this.openUrl}>
+        <Card onClick={() => {window.open(this.props.url, '_blank');}}>
           <CardImg top width="100%" src={`/images/${this.props.image}.jpg`} alt={this.props.image} />
           <CardBody>
             <CardText>{this.props.desc}</CardText>
